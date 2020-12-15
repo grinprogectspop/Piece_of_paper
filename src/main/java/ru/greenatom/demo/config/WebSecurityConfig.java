6109054e301040982bb8419b457bd458c7f90299
Bkmz <bkmzilbim@gmail.com>
2020-12-15 22:09:53 +0500

    Connect Swagger Web

--- src/main/java/ru/greenatom/demo/config/WebSecurityConfig.java
+++ src/main/java/ru/greenatom/demo/config/WebSecurityConfig.java
@@ -2,8 +2,8 @@ package ru.greenatom.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,9 +16,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userService)
-//                .passwordEncoder(passwordEncoder);
-//    }
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+       httpSecurity.csrf().disable();
+    }
 }

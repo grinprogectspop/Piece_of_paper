5abc5640c816e5bdbc513b33c779860e027aac37
Bkmz <bkmzilbim@gmail.com>
2020-12-16 11:59:07 +0500

    fix merge

--- src/main/java/ru/greenatom/demo/config/SwaggerConfig.java
+++ src/main/java/ru/greenatom/demo/config/SwaggerConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,10 +20,26 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select().apis(RequestHandlerSelectors.basePackage("guru.springframework.controllers"))
-                .paths(regex("/product.*"))
-                .build();
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("ru.greenatom.demo.controller"))
+                .paths(regex("/.*"))
+                .build()
+                .apiInfo(metaData());
+    }
 
+    /**
+     * todo  подробно изучить параметры title,description,licenseUrl,contact
+     *
+     * **/
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot REST API")
+                .description("\"Spring Boot REST API for Online Store\"")
+                .version("1.0.2")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(new Contact("John Thompson", "https://springframework.guru/about/", "john@springfrmework.guru"))
+                .build();
     }
 
     @Override

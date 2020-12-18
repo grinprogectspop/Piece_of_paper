e614729f3702046edcd72e6b96a567c383f4c02e
Bkmz <bkmzilbim@gmail.com>
2020-12-18 13:41:15 +0500

    Fix contact

--- src/main/java/ru/greenatom/demo/config/SwaggerConfig.java
+++ src/main/java/ru/greenatom/demo/config/SwaggerConfig.java
@@ -33,13 +33,17 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
      * **/
     private ApiInfo metaData() {
         return new ApiInfoBuilder()
-                .title("Spring Boot REST API")
+                .title("Spring Boot REST API Piece Of Paper")
                 .description("\"Spring Boot REST API for a piece of paper\"")
                 .version("1.0.2")
                 .license("Apache License Version 2.0")
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
                 .contact(new Contact("Project", "https://github.com/grinprogectspop/Piece_of_paper", " grinprogectspop@gmail.com"))
                 .contact(new Contact("Ветви", "https://github.com/grinprogectspop/Piece_of_paper/network", " grinprogectspop@gmail.com"))
+                .contact(new Contact("ECM.Spec.Req", "https://docs.google.com/document/d/1Cl_ODNYa6SD3kMDGC9Oe_ZAThl4ZbTUfSrDesAwvpw0/edit", " grinprogectspop@gmail.com"))
+                .contact(new Contact("API", "https://docs.google.com/document/d/1a_dS6EJJqt3a9z35XfsW9CUnLNWlNTuIuOnUZhHX0q0/edit?usp=sharing", " grinprogectspop@gmail.com"))
+                .contact(new Contact("Доска задач", "https://trello.com/invite/b/a5RVPYil/0a8bf0486a2dfc4731471282234dd1f8/проэкт", " grinprogectspop@gmail.com"))
+                .contact(new Contact("ER-диаграмма", "https://docs.google.com/document/d/1Cl_ODNYa6SD3kMDGC9Oe_ZAThl4ZbTUfSrDesAwvpw0/edit", " grinprogectspop@gmail.com"))
                 .build();
     }
 

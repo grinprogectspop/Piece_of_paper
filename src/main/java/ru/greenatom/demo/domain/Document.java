1b898815318d9e9d2940796b81e846d4eeb78832
Bkmz <bkmzilbim@gmail.com>
2020-12-18 20:26:06 +0500

    fix DB and sve document

--- src/main/java/ru/greenatom/demo/domain/Document.java
+++ src/main/java/ru/greenatom/demo/domain/Document.java
@@ -13,7 +13,6 @@ import java.time.LocalDateTime;
 import java.util.Set;
 
 @Entity
-@Data
 @ToString(of = { "documentId", "documentName" })
 @EqualsAndHashCode(of = { "documentId" })
 public class Document {
@@ -39,10 +38,10 @@ public class Document {
   @JsonView(Views.documents.class)
   private boolean deleted;
 
-//  @ElementCollection(targetClass = Action.class, fetch = FetchType.EAGER)
-//  @CollectionTable(name = "access", joinColumns = @JoinColumn(name = "document_id"))
-//  @Enumerated(EnumType.STRING)
-//  private Set<Action> accessTypes;
+  //  @ElementCollection(targetClass = Action.class, fetch = FetchType.EAGER)
+  //  @CollectionTable(name = "access", joinColumns = @JoinColumn(name = "document_id"))
+  //  @Enumerated(EnumType.STRING)
+  //  private Set<Action> accessTypes;
 
   @ManyToOne
   @JoinColumn(name = "document_type_id")
@@ -54,16 +53,89 @@ public class Document {
   private SecrecyLevel documentSecrecyLevel;
 
   @OneToMany(mappedBy = "document",
-          orphanRemoval = true,
-          cascade = CascadeType.ALL
+             orphanRemoval = true,
+             fetch = FetchType.EAGER,
+             cascade = CascadeType.ALL
   )
   @JsonView(Views.documents.class)
   private Set<DocumentHistory> changes;
 
   @OneToMany(mappedBy = "document",
-          orphanRemoval = true,
-          cascade = CascadeType.ALL
+             orphanRemoval = true,
+             cascade = CascadeType.ALL
   )
   @JsonView(Views.documents.class)
   private Set<DocumentVersion> versions;
+
+  public Long getDocumentId() {
+    return documentId;
+  }
+
+  public void setDocumentId(Long documentId) {
+    this.documentId = documentId;
+  }
+
+  public String getDocumentName() {
+    return documentName;
+  }
+
+  public void setDocumentName(String documentName) {
+    this.documentName = documentName;
+  }
+
+  public LocalDateTime getCreationDate() {
+    return creationDate;
+  }
+
+  public void setCreationDate(LocalDateTime creationDate) {
+    this.creationDate = creationDate;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public boolean isDeleted() {
+    return deleted;
+  }
+
+  public void setDeleted(boolean deleted) {
+    this.deleted = deleted;
+  }
+
+  public DocumentType getDocumentType() {
+    return documentType;
+  }
+
+  public void setDocumentType(DocumentType documentType) {
+    this.documentType = documentType;
+  }
+
+  public SecrecyLevel getDocumentSecrecyLevel() {
+    return documentSecrecyLevel;
+  }
+
+  public void setDocumentSecrecyLevel(SecrecyLevel documentSecrecyLevel) {
+    this.documentSecrecyLevel = documentSecrecyLevel;
+  }
+
+  public Set<DocumentHistory> getChanges() {
+    return changes;
+  }
+
+  public void setChanges(Set<DocumentHistory> changes) {
+    this.changes = changes;
+  }
+
+  public Set<DocumentVersion> getVersions() {
+    return versions;
+  }
+
+  public void setVersions(Set<DocumentVersion> versions) {
+    this.versions = versions;
+  }
 }
--- src/main/java/ru/greenatom/demo/services/DocumentServiceImpl.java
+++ src/main/java/ru/greenatom/demo/services/DocumentServiceImpl.java
@@ -60,7 +60,12 @@ public class DocumentServiceImpl implements DocumentService {
         documentHistory.setActionDate(LocalDateTime.now());
         documentHistory.setDocument(document);
         documentHistory.setDescription("setDescription");
+        documentHistory.setAuthor(userRepo.findOneByUserId(documentBuildingCreateModel.getUserId()));
+        Action action = Action.WRITE;
+
+        documentHistory.setActions(Collections.singleton(action));
         documentHistoryRepo.save(documentHistory);
+
 
 
         document.setChanges(Collections.singleton(documentHistory));
@@ -77,10 +82,8 @@ public class DocumentServiceImpl implements DocumentService {
 
         documentVersionRepo.save(documentVersion);
 
-        Action action = Action.WRITE;
 
-        documentHistory.setActions(Collections.singleton(action));
-
+        documentHistory.setDocumentVersion(documentVersion);
         documentHistoryRepo.save(documentHistory);
 
 
@@ -93,18 +96,24 @@ public class DocumentServiceImpl implements DocumentService {
         //Todo documentNew and documentNew не получается получить
         documentNew.setChanges(documentOld.getChanges());
         documentNew.setVersions(documentOld.getVersions());
+        documentNew.setDocumentName(documentOld.getDocumentName());
+
 
         DocumentVersion documentVersion = documentBuildingSaveModel.getVersionEdit();
 
         documentVersion.setDate(LocalDateTime.now());
         //Todo адекватный  URL
         documentVersion.setUrl("URL1");
+        documentVersion.setDocument(documentNew);
+        documentVersionRepo.save(documentVersion);
 
-        documentVersionRepo.save(documentBuildingSaveModel.getVersionEdit());
+
         DocumentHistory documentHistory = new DocumentHistory();
         documentHistory.setActionDate(LocalDateTime.now());
         documentHistory.setDocument(documentNew);
         documentHistory.setDescription("setDescription");
+        documentHistory.setAuthor(userRepo.findOneByUserId(documentBuildingSaveModel.getUserId()));
+        documentHistory.setDocumentVersion(documentVersion);
 
         documentNew.getChanges().add(documentHistory);
         documentHistoryRepo.save(documentHistory);
@@ -112,11 +121,12 @@ public class DocumentServiceImpl implements DocumentService {
 
         documentHistoryRepo.save(documentHistory);
         documentRepo.save(documentNew);
-        documentVersionRepo.save(documentVersion);
 
 
 
-        return null;
+
+
+        return documentNew;
     }
 
     @Override public Document delete(long documentId) {

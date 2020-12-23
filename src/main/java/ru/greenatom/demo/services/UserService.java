6f082e2182ba0b127fcdf91bf96d227974fc1dfd
Ambassador <idkhokhlov@edu.hse.ru>
2020-12-23 14:31:33 +0300

    Code refactoring #2

--- src/main/java/ru/greenatom/demo/services/UserService.java
+++
@@ -1,8 +0,0 @@
-package ru.greenatom.demo.services;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-import ru.greenatom.demo.models.binding.UserBuildingModel;
-
-public interface UserService extends UserDetailsService {
-    Long create(UserBuildingModel userBuildingModel);
-}
